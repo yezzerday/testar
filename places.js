9ae44e6347ffd254712efcfa9dde27a4d40874c8
yezzerday <107908675+yezzerday@users.noreply.github.com>
2024-10-05 03:11:52 +0700

    Update places.js

--- places.js
+++ places.js
@@ -1,26 +1,16 @@
 const places = [
-    { name: "คณะทันตแพทยศาสตร์", latitude:16.74656289880545, longitude: 100.18938550072663  },
-    { name: "คณะพยาบาลศาสตร์", latitude: 16.745467251821808 ,longitude: 100.18949878267634 },
-     { name: "คณะ", latitude:  longitude:  },
-     { name: "คณะrdest", latitude:  longitude: 100.19645815615301 },
-     { name: "คณะtest", latitude:, longitude: },
-     
-    // สถานที่อื่นๆ
-];
-
-const places = [
     {
         name: "Place 1",
         latitude: 16.751685403830074,
         longitude: 100.19655066388673,
         description: "Description of Place 1",
-        img: "https://via.placeholder.com/100"
+        img: "https://www.sa.nu.ac.th/wp-content/uploads/2023/11/17-4-%E0%B8%AA%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%99%E0%B9%89%E0%B8%B3.jpg"
     },
     {
         name: "Place 2",
         latitude: 16.75169538473392,
         longitude: 100.19696629722633,
         description: "Description of Place 2",
-        img: "https://via.placeholder.com/100"
+        img: "image/hospital.png"
     }
 ];
