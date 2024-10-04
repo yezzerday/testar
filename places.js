5f058397cc24bb468b7bc20d550d6a3a4eb8074f
yezzerday <107908675+yezzerday@users.noreply.github.com>
2024-10-05 03:08:59 +0700

    Update places.js

--- places.js
+++ places.js
@@ -1,10 +1,26 @@
 const places = [
     { name: "คณะทันตแพทยศาสตร์", latitude:16.74656289880545, longitude: 100.18938550072663  },
     { name: "คณะพยาบาลศาสตร์", latitude: 16.745467251821808 ,longitude: 100.18949878267634 },
-     { name: "คณะ", latitude: 16.751685403830074, longitude: 100.19655066388673 },
-     { name: "คณะrdest", latitude: 16.751838862716735, longitude: 100.19645815615301 },
-     { name: "คณะtest", latitude:16.75169538473392, longitude: 100.19696629722633},
+     { name: "คณะ", latitude:  longitude:  },
+     { name: "คณะrdest", latitude:  longitude: 100.19645815615301 },
+     { name: "คณะtest", latitude:, longitude: },
      
     // สถานที่อื่นๆ
 ];
 
+const places = [
+    {
+        name: "Place 1",
+        latitude: 16.751685403830074,
+        longitude: 100.19655066388673,
+        description: "Description of Place 1",
+        img: "https://via.placeholder.com/100"
+    },
+    {
+        name: "Place 2",
+        latitude: 16.75169538473392,
+        longitude: 100.19696629722633,
+        description: "Description of Place 2",
+        img: "https://via.placeholder.com/100"
+    }
+];
